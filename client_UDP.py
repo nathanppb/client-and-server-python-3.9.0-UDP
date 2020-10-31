a3004049266d56c2a6d06ac3e2cd2e810428044a
Nathan Peixoto <48286883+nathanppb@users.noreply.github.com>
2020-10-31 19:45:20 -0300

    Create client_UDP.py

---
+++ client_UDP.py
@@ -0,0 +1,13 @@
+import socket                                                           # Biblioteca de sockets Python
+ip = '127.0.0.1'       #input('digite o ip de conexao: ')
+port = 7000
+addr = ((ip,port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Cria o socket UDP
+mensagem = input("\nDigite o numero: ")                                 # Digitar a mensagem para ser enviada
+client_socket.sendto(mensagem.encode('utf-8'),(addr))                   # Envia a mensagem
+print ("\nmensagem enviada\n")                                          # Confirmacao que enviou
+Mensagem_enviar, serverAddress = client_socket.recvfrom(1024)           # Le o socket do SERVIDOR e pega IP do mesmo
+print ("IP DO SERVIDOR: ", serverAddress,"\n")                          # Mostra o IP e a Porta do Servidor
+print ("Retorno do servidor: ", Mensagem_enviar)                        # Mostra a mensagem que o Servidor Retornou
+
+client_socket.close()                                                   # Fecha o socket
